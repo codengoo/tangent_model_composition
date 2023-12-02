5ece5e69592fa5e06f75672a32045958480ef842
nghia <21020365@vnu.edu.vn>
2023-12-03 01:22:44 +0700

    UPDATE:

--- scripts/compose_cifar.sh
+++ scripts/compose_cifar.sh
@@ -2,7 +2,7 @@ dataset=cifar10 # choices: oxfordpets, caltech256, mit67
 n_tasks=5          # number of shards
 
 # Class Incremental Learning
-python main.py -m name=${dataset}-cil dataset=${dataset} +preset=tangent trainer=tangent mse_weight=15 n_tasks=${n_tasks}
+python main.py -m name=${dataset}-cil dataset=${dataset} +preset=tangent trainer=tangent mse_weight=15 n_tasks=${n_tasks} epochs=20
 
 # Data Incremental Learning
-python main.py -m name=${dataset}-dil dataset=${dataset} +preset=tangent trainer=tangent mse_weight=5 n_tasks=${n_tasks} task_split=rand
+python main.py -m name=${dataset}-dil dataset=${dataset} +preset=tangent trainer=tangent mse_weight=5 n_tasks=${n_tasks} task_split=rand epochs=20
